facdd10aada44a0f9edd0ae1df1b55d207e75e1d
Todd Ericskon <todd.erickson87@gmail.com>
2024-03-29 16:50:17 -0700

    Added React-Bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Button from 'react-bootstrap/Button';
+
+// See this for themeing https://getbootstrap.com/docs/5.3/customize/color-modes/#javascript
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,9 +21,9 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <Button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
