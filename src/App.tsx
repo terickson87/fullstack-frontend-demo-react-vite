60c577a051111d7357edc5ebeb04c02bfcc5eaa6
Todd Ericskon <todd.erickson87@gmail.com>
2024-04-09 16:10:36 -0700

    Revert "Added React-Bootstrap"
    This reverts commit facdd10aada44a0f9edd0ae1df1b55d207e75e1d.

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Button from 'react-bootstrap/Button';
-
-// See this for themeing https://getbootstrap.com/docs/5.3/customize/color-modes/#javascript
 
 function App() {
   const [count, setCount] = useState(0)
@@ -21,9 +18,9 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <Button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </Button>
+        </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
